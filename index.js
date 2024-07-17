c92e1631e3d69c3ab0850d6d5d2204134b112083
itzmesojib <itzmesojib@gmail.com>
2024-07-17 11:14:21 +0600

    emplement jwt, bcrypt

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient } = require('mongodb');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const app = express();
@@ -24,22 +26,87 @@ client.connect()
   .then(() => {
     console.log("Connected to MongoDB");
 
+    // Database collections
+    const usersCollection = client.db("QuickPayWallet").collection("users");
 
-  // Database collections
-  const usersCollection =  client.db("QuickPayWallet").collection("users");
-
-
+    // Generate JWT
+    const generateToken = (user) => {
+      return jwt.sign({ id: user._id, email: user.email }, process.env.JWT_SECRET, {
+        expiresIn: '1h', // Token expires in 1 hour
+      });
+    };
 
     // Routes
     app.get('/', (req, res) => {
       res.send('QuickPay Wallet Server Is Running');
     });
 
+    // Register user
+    app.post("/users", async (req, res) => {
+      try {
+        const newUser = req.body;
+
+        // Hash the password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(newUser.pin, salt);
+
+        // Replace the plain text password with the hashed password
+        newUser.pin = hashedPassword;
+
+        const result = await usersCollection.insertOne(newUser);
+        if (result.insertedId) {
+          const token = generateToken(newUser);
+          res.send({ token });
+        } else {
+          res.status(500).send('Error creating new user');
+        }
+      } catch (error) {
+        res.status(500).send('Error creating new user');
+      }
+    });
+
     // get all users
-    app.get("/users", async(req, res) => {
-        const allUsers = await usersCollection.find().toArray();
-        res.send(allUsers);
-    })
+    app.get("/users", async (req, res) => {
+      const users = await usersCollection.find().toArray();
+      res.send(users);
+    });
+
+
+    // User login
+    app.post('/login', async (req, res) => {
+      const { email, pin } = req.body;
+      const user = await usersCollection.findOne({ email });
+
+      if (!user) {
+        return res.status(400).send('User not found');
+      }
+
+      const isMatch = await bcrypt.compare(pin, user.pin);
+
+      if (!isMatch) {
+        return res.status(400).send('Invalid credentials');
+      }
+
+      const token = generateToken(user);
+      res.send({ token });
+    });
+
+    // Protected route example
+    app.get('/protected', (req, res) => {
+      const token = req.header('Authorization').replace('Bearer ', '');
+
+      if (!token) {
+        return res.status(401).send('Access denied');
+      }
+
+      try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        res.send('Protected data');
+      } catch (ex) {
+        res.status(400).send('Invalid token');
+      }
+    });
 
     // Start server
     app.listen(port, () => {
